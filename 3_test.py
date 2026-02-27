936b690944a6850849fb450206ceebc37aed557a
vdhpfleury <fleury.vdhp@gmail.com>
2026-02-27 12:40:21 +0100

    Update 3_test.py

--- 3_test.py
+++ 3_test.py
@@ -24,7 +24,7 @@ except ImportError:
     OIM_AVAILABLE = False
     st.error("oimodeler n'est pas installé. Installez-le avec : pip install oimodeler")
     st.stop()
-st.write(dir(oim))
+
 # ═══════════════════════════════════════════════════════════════════════════
 # 0.  Configuration de la page  (DOIT être le premier appel Streamlit)
 # ═══════════════════════════════════════════════════════════════════════════
@@ -2233,6 +2233,7 @@ with tab_model:
         "</div>",
         unsafe_allow_html=True,
     )
+
 
 
 
