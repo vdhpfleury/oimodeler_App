fb202291a18afd65c9031a3149c47494365ece25
vdhpfleury <fleury.vdhp@gmail.com>
2026-03-20 17:08:50 +0100

    Update 3_test.py

--- 3_test.py
+++ 3_test.py
@@ -1,10 +1,5 @@
 import sys, os
-sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
-
-from analytics import inject_ga
 import streamlit as st
-st.write(os.path.dirname(os.path.abspath(__file__)))
-st.write(os.listdir(os.path.dirname(os.path.abspath(__file__))))
 
 import io
 import re
